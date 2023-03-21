675bb1ecec807a6ccffbd77468de802d7366bebf
284794507@qq.com <284794507@qq.com>
2023-03-21 22:25:33 +0800

    init

---
+++ lib/utils/utils.go
@@ -0,0 +1,57 @@
+package utils
+
+//tocmdline convert strings to [][]byte
+func ToCmdLine(cmd ...string) [][]byte {
+	args := make([][]byte, len(cmd))
+	for i, s := range cmd {
+		args[i] = []byte(s)
+	}
+	return args
+}
+
+//tocmdline2 convert commandName and string-type argument to [][]byte
+func ToCmdline2(commandName string, args ...string) [][]byte {
+	result := make([][]byte, len(args)+1)
+	result[0] = []byte(commandName)
+	for i, s := range args {
+		result[i+1] = []byte(s)
+	}
+	return result
+}
+
+//tocmdline3 convert commandName and []byte-type argument to cmdline
+func ToCmdLine3(commandName string, args ...[]byte) [][]byte {
+	result := make([][]byte, len(args)+1)
+	result[0] = []byte(commandName)
+	for i, s := range args {
+		result[i+1] = s
+	}
+	return result
+}
+
+func Equals(a interface{}, b interface{}) bool {
+	sliceA, okA := a.([]byte)
+	sliceB, okB := b.([]byte)
+	if okA && okB {
+		return BytesEquals(sliceA, sliceB)
+	}
+	return a == b
+}
+
+func BytesEquals(a []byte, b []byte) bool {
+	if (a == nil && b != nil) || (a != nil && b == nil) {
+		return false
+	}
+	if len(a) != len(b) {
+		return false
+	}
+	size := len(a)
+	for i := 0; i < size; i++ {
+		av := a[i]
+		bv := b[i]
+		if av != bv {
+			return false
+		}
+	}
+	return true
+}
